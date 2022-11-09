da354d87a7414b5a5fd8990f5b225d9406ebc0c2
Jimin <77067383+wl2258@users.noreply.github.com>
2022-11-09 15:09:07 +0900

    좋아요 싫어요

--- app/src/main/java/kr/ac/kumoh/s20200607/w1001intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20200607/w1001intentdata/ImageActivity.kt
@@ -1,25 +1,54 @@
 package kr.ac.kumoh.s20200607.w1001intentdata
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
 import android.widget.Toast
 import kr.ac.kumoh.s20200607.w1001intentdata.databinding.ActivityImageBinding
 
-class ImageActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity(),
+    View.OnClickListener {
+        companion object {
+            const val imageName = "image"
+            const val resultName = "result"
+
+            const val LIKE = 10
+            const val DISLIKE = 20
+            const val NONE = 0
+        }
     private lateinit var binding: ActivityImageBinding
+    private var image: String? = null
+
+    override fun onClick(v: View?) {
+        val intent = Intent()
+        val value = when (v?.id) {
+            binding.btnLike.id -> LIKE
+            binding.btnDislike.id -> DISLIKE
+            else -> NONE
+        }
+        intent.putExtra(imageName, image)
+        intent.putExtra(resultName, value)
+        setResult(RESULT_OK, intent)
+        finish() //현재 액티비티 종료해라!
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_image)
         binding = ActivityImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Toast.makeText(this, intent.getStringExtra("image"), Toast.LENGTH_LONG).show()
-
-        val res = when (intent.getStringExtra("image")) {
+        image = intent.getStringExtra(MainActivity.keyName)
+        val res = when (image){
             "gundam" -> R.drawable.gundam
             "zaku" -> R.drawable.zaku
             else -> R.drawable.ic_launcher_foreground
         }
         binding.imgGundam.setImageResource(res)
+
+        binding.btnLike.setOnClickListener(this)
+        binding.btnDislike.setOnClickListener(this)
     }
 }
--- app/src/main/java/kr/ac/kumoh/s20200607/w1001intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20200607/w1001intentdata/MainActivity.kt
@@ -3,27 +3,81 @@ package kr.ac.kumoh.s20200607.w1001intentdata
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import kr.ac.kumoh.s20200607.w1001intentdata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(),
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.btnGundam.setOnClickListener {
+//            val intent = Intent(this, ImageActivity::class.java)
+//            intent.putExtra("image", "gundam") //intent data 전달 시 사용
+//            startActivity(intent)
+//        }
+//        binding.btnZaku.setOnClickListener {
+//            val intent = Intent(this, ImageActivity::class.java)
+//            intent.putExtra("image", "zaku")
+//            startActivity(intent)
+//        }
+//    }
+
+    View.OnClickListener { //onclicklistener은 인터페이스
+        private lateinit var binding: ActivityMainBinding
+        private lateinit var launcher: ActivityResultLauncher<Intent> //
+
+        companion object {
+            const val keyName = "image"
+        }
+        override fun onClick(v: View?) {
+            val intent = Intent(this, ImageActivity::class.java)
+            val value = when (v?.id) {
+                binding.btnGundam.id -> "gundam"
+                binding.btnZaku.id -> "zaku"
+                else -> return
+            }
+            intent.putExtra(keyName, value) //intent data 전달 시 사용
+            launcher.launch(intent)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnGundam.setOnClickListener {
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra("image", "gundam") //intent data 전달 시 사용
-            startActivity(intent)
-        }
+        binding.btnGundam.setOnClickListener(this) //내 객체를 넣어줌
+        binding.btnZaku.setOnClickListener(this)
 
-        binding.btnZaku.setOnClickListener {
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra("image", "zaku")
-            startActivity(intent)
-        }
+        /*
+            * 교수님도 익숙하지 않다며,,, 풀 수 있는 것만 시험에 내는데,,, 이건 모르겠다 충!격!발!언!
+         */
+        launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) { // callback 함수 같이 넣어줌. 여기로 돌아오도록
 
+            if (it.resultCode != RESULT_OK)
+                return@registerForActivityResult //어디까지 뭘 리턴하려는지 알아서 알려줌
+
+            val result = it.data?.getIntExtra(ImageActivity.resultName, ImageActivity.NONE) //data 안에 intent가 들어있음.
+            //(getIntExtra에서만) 만약 resultName이 존재하지 않는다면 어떤 값을 반환할지 지정해줄 수 있음 -> ImageActivity.NONE
+
+            val str = when (result) {
+                ImageActivity.LIKE -> "좋아요"
+                ImageActivity.DISLIKE -> "싫어요"
+                else -> ""
+            }
+            val image = it.data?.getStringExtra(ImageActivity.imageName)
+            when (image) {
+                "gundam" -> binding.btnGundam.text = "건담 ($str)"
+                "zaku" -> binding.btnZaku.text = "자쿠 ($str)"
+            }
+        }
     }
-}
+    }
+
